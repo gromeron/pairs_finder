d04e1d9a8442afef90c82b539d78a39fd1ed5000
Gustavo Romero <g.romeron2@uniandes.edu.co>
2022-12-08 12:31:40 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+def pairs_finder(arr, sum):
+
+    s1 = arr
+    s2 = {}
+
+    for i, v in enumerate(s1):
+        diff = sum - v
+        if diff in s1:
+            s2[v] = diff
+    return s2
+
+def show_results(s2):
+    for key, values in s2.items():
+        print("+ ",key,",",values)
+
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input('> Enter comma separated integers: ').rstrip().split(',')))
+    
+    sum = int(input('> Please enter the sum value: ').strip())
+
+    result = pairs_finder(arr, sum)
+
+    print(f'the solution 1 is: {result}')
+    show_results(result)
